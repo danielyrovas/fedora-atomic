e1f154225a6d1cc7d1913f46cfd981287fa7f433
Daniel Yrovas <daniel@yrovas.com>
2024-04-24 12:37:29 +1000

    install steam & lutris

--- build.sh
+++ build.sh
@@ -4,20 +4,21 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+wget https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-"${RELEASE}"/wezfurlong-wezterm-nightly-"${RELEASE}".repo -O /etc/yum.repos.d/_copr_wezterm-nightly.repo
 
-### Install packages
+wget https://copr.fedorainfracloud.org/coprs/kylegospo/system76-scheduler/repo/fedora-"${RELEASE}"/kylegospo-system76-scheduler-fedora-"${RELEASE}".repo -O /etc/yum.repos.d/_copr_kylegospo-system76-scheduler.repo
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Bazzite game setup
+# wget https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite/repo/fedora-"${RELEASE}"/kylegospo-bazzite-fedora-"${RELEASE}".repo -O /etc/yum.repos.d/_copr_kylegospo-bazzite.repo
+# wget https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite-multilib/repo/fedora-"${RELEASE}"/kylegospo-bazzite-multilib-fedora-"${RELEASE}".repo?arch=x86_64 -O /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo
+wget https://copr.fedorainfracloud.org/coprs/kylegospo/rom-properties/repo/fedora-"${RELEASE}"/kylegospo-rom-properties-fedora-"${RELEASE}".repo -O /etc/yum.repos.d/_copr_kylegospo-rom-properties.repo
 
-# this installs a package from fedora repos
-EXCLUDED="bcache-tools fish gcc glow libimobiledevice stress-ng tailscale tmux usbmuxd openssh-askpass gnome-shell-extension-tailscale-gnome-q"
 
-EXTRA="openssh-askpass-gnome"
+REMOVE="bcache-tools fish gcc glow stress-ng tailscale tmux gnome-shell-extension-tailscale-gnome-qs gamemode"
 
-rpm-ostree override remove $EXCLUDED
+rpm-ostree override remove $REMOVE
+
+EXTRA="wezterm rom-properties-gtk3 switcheroo-control system76-scheduler btop wmctrl vulkan-tools steam lutris wine wine-core winetricks protontricks mangohud"
 
 rpm-ostree install $EXTRA
 
